1319aced3ceb1e0023091afa8dd41bde4cc4c611
0x5c <dev@0x5c.io>
2022-06-26 03:33:15 -0400

    1.1.0: Upstream format update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,10 +7,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ## [Unreleased]
 
 
+## [1.1.0] - 2022-06-26
+### Changed
+- Parsing and formatting updated to match the new format of void-robot.
+
+
 ## [1.0.0] - 2022-01-01
 ### Added
 - Reformatting of void-robot notices in #xbps.
 
 
-[Unreleased]: https://github.com/0x5c/hexchat-void-repos/compare/v1.0.0...HEAD
+[Unreleased]: https://github.com/0x5c/hexchat-void-repos/compare/v1.1.0...HEAD
+[1.1.0]: https://github.com/0x5c/hexchat-void-repos/releases/tag/v1.1.0
 [1.0.0]: https://github.com/0x5c/hexchat-void-repos/releases/tag/v1.0.0
--- void_repos.py
+++ void_repos.py
@@ -11,7 +11,7 @@ import hexchat
 
 
 __module_name__ = "void-repos"
-__module_version__ = "1.0.0"
+__module_version__ = "1.1.0"
 __module_description__ = "Plugin for Voidlinux's git repositories"
 debug = False
 
@@ -22,7 +22,8 @@ def handle_robot(word: list[str], word_eol: list[str], userdata):
             if word[0] == "void-robot":
                 event, title, url = parse_robot(word[2])
                 hexchat.prnt(f"\00311Void\00310Robot\t\x0f{event}")
-                hexchat.prnt(f"\00310{title}")
+                if title:
+                    hexchat.prnt(f"\00310{title}")
                 if url:
                     hexchat.prnt(f"\00314{url}")
                 return hexchat.EAT_HEXCHAT
@@ -33,7 +34,8 @@ if debug:
     def robot_test(word: list[str], word_eol: list[str], userdata):
         event, title, url = parse_robot(word_eol[1])
         hexchat.prnt(f"\00311Void\00310Robot\t\x0f{event}")
-        hexchat.prnt(f"\00310{title}")
+        if title:
+            hexchat.prnt(f"\00310{title}")
         if url:
             hexchat.prnt(f"\00314{url}")
         return hexchat.EAT_ALL
@@ -49,28 +51,48 @@ hexchat.prnt("\00311Void\00310Repos\tPlugin loaded!")
 # PR:
 #   steinex opened #34796 [void-packages] (cozy: update to 1.1.3, adopt)
 # Push:
-#   Hoshpak pushed to void-packages (linux4.19: update to 4.19.223.)
+#   classabbyamp pushed 5 commits to test (db6b6029 -> 9557e5ed, new HEAD: add something)
+#   classabbyamp pushed 1 commit to test (9557e5ed -> f09714d3, new HEAD: change something)
+# Force-push:
+#   classabbyamp force-pushed 5 commits to test (fe737118 -> cad7271e, new HEAD: add something)
+#   classabbyamp force-pushed 1 commit to test (f09714d3 -> 2d47b36f, new HEAD: change something)
 
 
 def parse_robot(msg: str) -> tuple[str, str, str]:
-    user, msg = msg.split(" ", 1)
-    if msg.startswith(("opened", "closed")):
-        action, msg = msg.split(" ", 1)
-        if action == "opened":
-            action = "\00303" + action
-        else:
-            action = "\00305" + action
-        number, msg = msg.split(" ", 1)
-        number = number.removeprefix("#")
-        repo, msg = msg.split(" ", 1)
-        repo = repo.removeprefix("[").removesuffix("]")
-        title = msg.removeprefix("(").removesuffix(")")
-        return (f"\00312{user} {action} \00313#{number} \00311in \00312{repo}",
-                title,
-                f"https://github.com/void-linux/{repo}/pull/{number}")
-    if msg.startswith("pushed"):
-        msg = msg.removeprefix("pushed to ")
-        repo, msg = msg.split(" ", 1)
-        title = msg.removeprefix("(").removesuffix(")")
-        return (f"\00312{user} \00307pushed \00311to \00312{repo}",
-                title, "")
+    orig_msg = msg
+    try:
+        user, msg = msg.split(" ", 1)
+    except ValueError:
+        user, msg = "", orig_msg
+    else:
+        if msg.startswith(("opened", "closed")):
+            action, msg = msg.split(" ", 1)
+            if action == "opened":
+                action = "\00303" + action
+            else:
+                action = "\00305" + action
+            number, msg = msg.split(" ", 1)
+            number = number.removeprefix("#")
+            repo, msg = msg.split(" ", 1)
+            repo = repo.removeprefix("[").removesuffix("]")
+            title = msg.removeprefix("(").removesuffix(")")
+            return (f"\00312{user} {action} \00313#{number} \00311in \00312{repo}",
+                    title,
+                    f"https://github.com/void-linux/{repo}/pull/{number}")
+        elif msg.startswith(("pushed", "force-pushed")):
+            action, msg = msg.split(" ", 1)
+            if action == "pushed":
+                action = "\00307" + action
+            else:
+                action = "\00304" + action
+            num_commits, msg = msg.split(" ", 1)
+            commit_word, msg = msg.split(" ", 1)
+            msg = msg.removeprefix("to ")
+            repo, msg = msg.split(" ", 1)
+            hashes, title = msg.removeprefix("(").removesuffix(")").split(", ", 1)
+            title = title.removeprefix("new HEAD: ")
+            before, after = hashes.split(" -> ")
+            return (f"\00312{user} {action} \00308{num_commits} \00311{commit_word} \00311to \00312{repo}",
+                    f"\00314{after}: \00310{title}",
+                    f"https://github.com/void-linux/{repo}/compare/{before}...{after}")
+    return (f"\00311{orig_msg}", "", "")
